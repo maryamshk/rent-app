19ac4a541f34e596678ae4bbab268886259dbe3c
maryam javed <maryamjavedshk@gmail.com>
2025-03-09 01:32:05 +0500

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = 5000;
-const mongoDB = require('./db');
-const cors = require('cors');
+const mongoDB = require("./db");
+const cors = require("cors");
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
@@ -17,12 +17,11 @@ mongoDB();
 app.use(cors());
 
 app.use(express.json());
-app.use('/api', require('./Routes/AuthRoute'));
-app.use('/api', require('./Routes/ProductRoute'));
-app.use('/api', require('./Routes/CategoryRoute'))
-app.use('/api', require('./Routes/OrderRoute'))
-
+app.use("/api", require("./Routes/AuthRoute"));
+app.use("/api", require("./Routes/ProductRoute"));
+app.use("/api", require("./Routes/CategoryRoute"));
+app.use("/api", require("./Routes/OrderRoute"));
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
-})
+  console.log(`Listening on port ${port}`);
+});
